8cceba73beeebbf26f7edf90ba38a546dd7a3080
Tiago Resende <tiagor87@gmail.com>
2019-03-03 13:38:30 -0300

    Atualiza arquivo de teste

--- test.cmd
+++ test.cmd
@@ -1,4 +1,4 @@
-nuget install xunit.runner.console -OutputDirectory packages -Version 2.4.1
+#nuget install xunit.runner.console -OutputDirectory packages -Version 2.4.1
 
 nuget install OpenCover -OutputDirectory packages -Version 4.7.922
 
@@ -6,6 +6,7 @@ dotnet tool install coveralls.net --tool-path packages
 
 dotnet build -c Release /p:DebugType=Full
 
-.\packages\OpenCover.4.7.922\tools\OpenCover.Console.exe -register:user -target:dotnet.exe "-targetargs:"".\packages\xunit.runner.console.2.4.1\tools\netcoreapp2.0\xunit.console.dll"" "".\tests\UnitTests\bin\Release\netcoreapp2.2\ResultCore.UnitTests.dll"" -noshadow -appveyor" -filter:"+[ResultCore]* -[ResultCore.UnitTests]*" -oldStyle -output:"coverage.xml"
+.\packages\OpenCover.4.7.922\tools\OpenCover.Console.exe -target:"dotnet.exe" -targetargs:"test \"tests\UnitTests\ResultCore.UnitTests.csproj\" --configuration Debug --no-build" -filter:"+[ResultCore]* -[ResultCore*Tests]*" -oldStyle -register:user -output:"coverage.xml"
+#.\packages\OpenCover.4.7.922\tools\OpenCover.Console.exe -register:user -target:"dotnet.exe" "-targetargs:"".\packages\xunit.runner.console.2.4.1\tools\netcoreapp2.0\xunit.console.dll"" "".\tests\UnitTests\bin\Release\netcoreapp2.2\ResultCore.UnitTests.dll"" -noshadow -appveyor" -filter:"+[ResultCore]* -[ResultCore.UnitTests]*" -oldStyle -output:"coverage.xml"
 
 .\packages\csmacnz.coveralls.exe --opencover -i coverage.xml --repoToken $env:COVERALLS_REPO_TOKEN
