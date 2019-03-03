fcecb4b0b10b48c4b7efe426b8c4174f44855eda
Tiago Resende <tiagor87@gmail.com>
2019-03-03 13:11:05 -0300

    Atualiza arquivo de teste

--- test.cmd
+++ test.cmd
@@ -4,6 +4,8 @@ nuget install OpenCover -OutputDirectory packages -Version 4.7.922
 
 dotnet tool install coveralls.net --tool-path packages
 
-.\packages\OpenCover.4.7.922\tools\OpenCover.Console.exe -register:user -target:dotnet.exe "-targetargs:"".\packages\xunit.runner.console.2.4.1\tools\netcoreapp2.0\xunit.console.dll"" "".\tests\UnitTests\bin\Release\netcoreapp2.0\ResultCore.UnitTests.dll"" -noshadow -appveyor" -filter:"+[ResultCore]* -[ResultCore.UnitTests]*" -oldStyle -output:coverage.xml
+dotnet build -c Release
 
-.\packages\csmacnz.coveralls.exe --opencover -i coverage.xml --repoToken $env:COVERALLS_REPO_TOKEN --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_JOB_ID
+.\packages\OpenCover.4.7.922\tools\OpenCover.Console.exe -register:user -target:dotnet.exe "-targetargs:"".\packages\xunit.runner.console.2.4.1\tools\netcoreapp2.0\xunit.console.dll"" "".\tests\UnitTests\bin\Release\netcoreapp2.2\ResultCore.UnitTests.dll"" -noshadow -appveyor" -filter:"+[ResultCore]* -[ResultCore.UnitTests]*" -oldStyle -output:"coverage.xml"
+
+.\packages\csmacnz.coveralls.exe --opencover -i coverage.xml --repoToken $env:COVERALLS_REPO_TOKEN
