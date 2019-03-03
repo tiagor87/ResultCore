35b26c12a5d88e8bac0d5db043be823420e2c26c
Tiago Resende <tiagor87@gmail.com>
2019-03-03 13:02:47 -0300

    Atualiza arquivo appveyor

--- test.cmd
+++ test.cmd
@@ -1,6 +1,9 @@
-nuget install OpenCover -Version 4.6.519 -OutputDirectory coverage
-nuget install coveralls.net -Version 0.6.0 -OutputDirectory coverage
-dotnet restore .\tests\UnitTests\
-dotnet build .\tests\UnitTests\
-.\coverage\OpenCover.4.6.519\tools\Opencover.console.exe -register:user -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:"test "".\tests\UnitTests"" -f netcoreapp2.0" -filter:"+[*]* -[*Tests]*" -returntargetcode -output:"coverage.xml" -oldStyle
-.\coverage\coveralls.net.0.6.0\tools\csmacnz.Coveralls.exe --opencover -i .\coverage.xml
+nuget install xunit.runner.console -OutputDirectory packages -Version 2.4.1
+
+nuget install OpenCover -OutputDirectory packages -Version 4.7.922
+
+dotnet tool install coveralls.net --tool-path packages
+
+.\packages\OpenCover.4.7.922\tools\OpenCover.Console.exe -register:user -target:dotnet.exe "-targetargs:"".\packages\xunit.runner.console.2.4.1\tools\netcoreapp2.0\xunit.console.dll"" "".\tests\UnitTests\bin\Release\netcoreapp2.0\ResultCore.UnitTests.dll"" -noshadow -appveyor" -filter:"+[ResultCore]* -[ResultCore.UnitTests]*" -oldStyle -output:coverage.xml
+
+.\packages\csmacnz.coveralls.exe --opencover -i coverage.xml --repoToken $env:COVERALLS_REPO_TOKEN --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_JOB_ID
