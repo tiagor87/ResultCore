04208b1495cfc15b38cc873029a28a21e7080b03
Tiago Resende <tiagor87@gmail.com>
2019-03-20 19:07:54 -0300

    Fix issues

--- src/Core/Result.cs
+++ src/Core/Result.cs
@@ -6,8 +6,8 @@ namespace ResultCore
 {
     public sealed class Result
     {
-        private readonly object _value; 
-        
+        private readonly object _value;
+
         private Result()
         {
             _value = null;
@@ -24,8 +24,8 @@ namespace ResultCore
         {
             Message = message;
         }
-        
-        private Result(IEnumerable<string> messages) : this (string.Join("\n", messages))
+
+        private Result(IEnumerable<string> messages) : this(string.Join("\n", messages))
         {
         }
 
@@ -33,7 +33,8 @@ namespace ResultCore
             Successful
                 ? _value
                 : throw new InvalidOperationException(
-            $"The result has failed, it's not possible to get value from it.\nMessage: \"{Message}\".");
+                    $"The result has failed, it's not possible to get value from it.\nMessage: \"{Message}\".");
+
         public bool Successful { get; }
         public string Message { get; }
         public bool Failure => !Successful;
@@ -47,48 +48,68 @@ namespace ResultCore
 
         public static Result Combine(params Result[] results)
         {
+            return Combine(results.ToList());
+        }
+
+        public static Result Combine(IReadOnlyList<Result> results)
+        {
             if (results.Any(x => !x.Successful))
             {
                 return Fail(results.Where(x => !x.Successful).Select(x => x.Message));
             }
 
-            var genericType = Type.GetType($"System.ValueTuple`{results.Length}");
-            var typeArgs = results.Select(x => x.Value.GetType()).ToArray();
+            var genericType = GetValueTupleType(results.Count);
+            var typeArgs = results.Select(x => x.Value?.GetType() ?? typeof(object)).ToArray();
             var specificType = genericType.MakeGenericType(typeArgs);
             var constructorArguments = results.Select(x => x.As<object>()).ToArray();
             return Success(Activator.CreateInstance(specificType, constructorArguments));
         }
+
+        private static Type GetValueTupleType(int count)
+        {
+            switch (count)
+            {
+                case 1: return typeof(ValueTuple<>);
+                case 2: return typeof(ValueTuple<,>);
+                case 3: return typeof(ValueTuple<,,>);
+                case 4: return typeof(ValueTuple<,,,>);
+                case 5: return typeof(ValueTuple<,,,,>);
+                case 6: return typeof(ValueTuple<,,,,,>);
+                case 7: return typeof(ValueTuple<,,,,,,>);
+                default: throw new NotSupportedException("Combine more than 7 elements are not supported.");
+            }
+        }
     }
 
-    public sealed class Result<TValue> 
+    public sealed class Result<TValue>
     {
         private readonly TValue _value;
-        
+
         private Result(TValue value)
         {
             Successful = true;
             _value = value;
         }
-        
+
         private Result(string message)
         {
             Message = message;
         }
-        
-        private Result(IEnumerable<string> messages) : this (string.Join("\n", messages))
+
+        private Result(IEnumerable<string> messages) : this(string.Join("\n", messages))
         {
         }
-        
+
         public bool Successful { get; }
         public bool Failure => !Successful;
         public string Message { get; }
-        
+
         public TValue Value =>
             Successful
                 ? _value
                 : throw new InvalidOperationException(
                     $"The result has failed, it's not possible to get value from it.\nMessage: \"{Message}\".");
-        
+
         public static Result<TValue> Success(TValue value) => new Result<TValue>(value);
         public static Result<TValue> Fail(string message) => new Result<TValue>(message);
         public static Result<TValue> Fail(IEnumerable<string> messages) => new Result<TValue>(messages);
--- tests/UnitTests/ResultTests.cs
+++ tests/UnitTests/ResultTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using FluentAssertions;
 using Xunit;
 
@@ -6,10 +7,49 @@ namespace ResultCore.UnitTests
 {
     public class ResultTests
     {
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        public void Should_combine_results(int count)
+        {
+            var results = new List<Result>();
+            for (int i = 0; i < count; i++)
+            {
+                results.Add(Result.Success());
+            }
+
+            var result = Result.Combine(results);
+
+            result.Should().NotBeNull();
+            result.Successful.Should().BeTrue();
+            result.Failure.Should().BeFalse();
+            result.Message.Should().BeNullOrWhiteSpace();
+        }
+
         [Fact]
         public void Should_combine_failure_results()
         {
             var result = Result.Combine(Result.Fail("Error 1"), Result.Success(1), Result.Fail("Error 2"));
+
+            result.Should().NotBeNull();
+            result.Successful.Should().BeFalse();
+            result.Failure.Should().BeTrue();
+            result.Message.Should().NotBeNullOrWhiteSpace();
+            result.Message.Should()
+                .Contain("Error 1")
+                .And
+                .Contain("Error 2");
+        }
+
+        [Fact]
+        public void Should_combine_failure_results_and_typed_results()
+        {
+            var result = Result.Combine(Result.Fail("Error 1"), Result<long>.Success(1), Result<long>.Fail("Error 2"));
 
             result.Should().NotBeNull();
             result.Successful.Should().BeFalse();
@@ -37,6 +77,22 @@ namespace ResultCore.UnitTests
         }
 
         [Fact]
+        public void Should_combine_successful_results_and_typed_results()
+        {
+            var result = Result.Combine(Result.Success("Value"), Result<int>.Success(1),
+                Result<DateTime>.Success(DateTime.UtcNow));
+
+            var (text, number, date) = result.As<(string, int, DateTime)>();
+            result.Should().NotBeNull();
+            result.Successful.Should().BeTrue();
+            result.Failure.Should().BeFalse();
+            result.Message.Should().BeNullOrWhiteSpace();
+            text.Should().Be("Value");
+            number.Should().Be(1);
+            date.Should().BeCloseTo(date, TimeSpan.FromSeconds(1));
+        }
+
+        [Fact]
         public void Should_create_failure_result_with_message()
         {
             var result = Result.Fail("Error message");
@@ -46,11 +102,36 @@ namespace ResultCore.UnitTests
             result.Failure.Should().BeTrue();
             result.Message.Should().Be("Error message");
         }
-        
+
         [Fact]
         public void Should_create_failure_result_with_messages()
         {
-            var result = Result.Fail(new string[] { "Error message", "Error message 2" });
+            var result = Result.Fail(new string[] {"Error message", "Error message 2"});
+
+            result.Should().NotBeNull();
+            result.Successful.Should().BeFalse();
+            result.Failure.Should().BeTrue();
+            result.Message.Should()
+                .Contain("Error message")
+                .And
+                .Contain("Error message 2");
+        }
+
+        [Fact]
+        public void Should_create_failure_typed_result_with_message()
+        {
+            var result = Result<long>.Fail("Error message");
+
+            result.Should().NotBeNull();
+            result.Successful.Should().BeFalse();
+            result.Failure.Should().BeTrue();
+            result.Message.Should().Be("Error message");
+        }
+
+        [Fact]
+        public void Should_create_failure_typed_result_with_messages()
+        {
+            var result = Result<long>.Fail(new string[] {"Error message", "Error message 2"});
 
             result.Should().NotBeNull();
             result.Successful.Should().BeFalse();
@@ -73,6 +154,20 @@ namespace ResultCore.UnitTests
         }
 
         [Fact]
+        public void Should_get_successful_typed_value()
+        {
+            const long value = 10;
+            var result = Result<long>.Success(value);
+
+            result.Should().NotBeNull();
+            result.Successful.Should().BeTrue();
+            result.Failure.Should().BeFalse();
+            result.Message.Should().BeNullOrWhiteSpace();
+
+            result.Value.Should().Be(value);
+        }
+
+        [Fact]
         public void Should_get_successful_value()
         {
             var result = Result.Success("Value");
@@ -85,49 +180,10 @@ namespace ResultCore.UnitTests
         }
 
         [Fact]
-        public void Should_get_successful_typed_value()
-        {
-            const long value = 10;
-            var result = Result<long>.Success(value);
-
-            result.Should().NotBeNull();
-            result.Successful.Should().BeTrue();
-            result.Failure.Should().BeFalse();
-            result.Message.Should().BeNullOrWhiteSpace();
-            
-            result.Value.Should().Be(value);
-        }
-        
-        [Fact]
-        public void Should_create_failure_typed_result_with_message()
-        {
-            var result = Result<long>.Fail("Error message");
-
-            result.Should().NotBeNull();
-            result.Successful.Should().BeFalse();
-            result.Failure.Should().BeTrue();
-            result.Message.Should().Be("Error message");
-        }
-        
-        [Fact]
-        public void Should_create_failure_typed_result_with_messages()
-        {
-            var result = Result<long>.Fail(new string[] { "Error message", "Error message 2" });
-
-            result.Should().NotBeNull();
-            result.Successful.Should().BeFalse();
-            result.Failure.Should().BeTrue();
-            result.Message.Should()
-                .Contain("Error message")
-                .And
-                .Contain("Error message 2");
-        }
-
-        [Fact]
         public void Should_implicit_convert_typed_result_to_result()
         {
             const string value = "Value";
-            
+
             Result result = Result<string>.Success(value);
 
             result.Successful.Should().BeTrue();
@@ -136,35 +192,21 @@ namespace ResultCore.UnitTests
             result.Value.Should().BeOfType<string>();
             result.As<string>().Should().Be(value);
         }
-        
-        [Fact]
-        public void Should_combine_failure_results_and_typed_results()
-        {
-            var result = Result.Combine(Result.Fail("Error 1"), Result<long>.Success(1), Result<long>.Fail("Error 2"));
-
-            result.Should().NotBeNull();
-            result.Successful.Should().BeFalse();
-            result.Failure.Should().BeTrue();
-            result.Message.Should().NotBeNullOrWhiteSpace();
-            result.Message.Should()
-                .Contain("Error 1")
-                .And
-                .Contain("Error 2");
-        }
 
         [Fact]
-        public void Should_combine_successful_results_and_typed_results()
+        public void Should_throw_when_combine_more_than_seven_elements()
         {
-            var result = Result.Combine(Result.Success("Value"), Result<int>.Success(1), Result<DateTime>.Success(DateTime.UtcNow));
+            Func<Result> action = () => Result.Combine(
+                Result.Success(),
+                Result.Success(),
+                Result.Success(),
+                Result.Success(),
+                Result.Success(),
+                Result.Success(),
+                Result.Success(),
+                Result.Success());
 
-            var (text, number, date) = result.As<(string, int, DateTime)>();
-            result.Should().NotBeNull();
-            result.Successful.Should().BeTrue();
-            result.Failure.Should().BeFalse();
-            result.Message.Should().BeNullOrWhiteSpace();
-            text.Should().Be("Value");
-            number.Should().Be(1);
-            date.Should().BeCloseTo(date, TimeSpan.FromSeconds(1));
+            action.Should().Throw<NotSupportedException>();
         }
 
         [Fact]
@@ -177,14 +219,14 @@ namespace ResultCore.UnitTests
             action.Should().Throw<InvalidOperationException>();
             result.Invoking(x => x.As<object>()).Should().Throw<InvalidOperationException>();
         }
-        
+
         [Fact]
         public void Should_throw_when_get_value_from_failure_in_typed_result()
         {
             var result = Result<long>.Fail("Error message");
 
             Func<long> action = () => result.Value;
-            
+
             action.Should().Throw<InvalidOperationException>();
         }
     }
